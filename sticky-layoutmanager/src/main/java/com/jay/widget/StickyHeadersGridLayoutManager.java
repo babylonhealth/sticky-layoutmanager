db5ad1078847c5a89da012b2347932caa3790d78
Sergei Chelombitko <sergei.chelombitko@babylonhealth.com>
2020-10-02 13:16:00 +0100

    Migrate to AndroidX

--- sticky-layoutmanager/src/main/java/com/jay/widget/StickyHeadersGridLayoutManager.java
+++ sticky-layoutmanager/src/main/java/com/jay/widget/StickyHeadersGridLayoutManager.java
@@ -4,14 +4,14 @@ import android.content.Context;
 import android.graphics.PointF;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewTreeObserver;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
